b5ac3dc17513b6524e4e49938c3465a0d0125070
ParthMishra20 <124814034+ParthMishra20@users.noreply.github.com>
2023-07-30 16:48:50 +0530

    update

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './header.css';
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="project_header" id="home">
+      <div className="project_header content">
+        <h1 classname="gradient_text">LEARN REACT</h1>
+      </div>
+
+    </div>
   )
 }
 
