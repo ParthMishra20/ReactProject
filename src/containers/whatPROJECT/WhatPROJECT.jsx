3663e63822d967a0f5169b2ab2d5194b211ab629
ParthMishra20 <124814034+ParthMishra20@users.noreply.github.com>
2023-08-11 01:50:27 +0530

    Update WhatPROJECT.jsx

--- src/containers/whatPROJECT/WhatPROJECT.jsx
+++ src/containers/whatPROJECT/WhatPROJECT.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import './whatPROJECT.css'
+import { Feature } from '../../components'
 const WhatPROJECT = () => {
   return (
-    <div className="">
+    <div className="project_section-margin" id="what">
+      <div className="project-feature">
+        <Feature />
+      </div>
+      <div className="project_heading">
+        <h1 className="gradient-text">OFFERS AND PROMOTION</h1>
+      </div>
     </div>
   )
 }
